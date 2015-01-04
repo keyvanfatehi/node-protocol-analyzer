320f60add69663b2d073cf65719b500ef645138b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-01-04 00:13:00 -0800

    limit to 2 ports

--- client/index.js
+++ client/index.js
@@ -1,2 +1,16 @@
 window.$ = require('jquery')
 window.socket = require('./socket')
+
+$('#ports input').change(function(e) {
+  var ports = getSelectedPorts();
+  if (ports.length > 2) $(e.target).prop('checked', false);
+})
+
+$('button#start').click(function() {
+  var ports = getSelectedPorts()
+  $('#ports input:checked')
+})
+
+function getSelectedPorts() {
+  return $('#ports input:checked').map(function(i,e){ return e.value })
+}
